245f9584b49d9e259fd503c19ebbf63904600603
Matt Henderson <mattcharlesh@gmail.com>
2024-05-16 13:55:44 -0400

    add book form with validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import { Typography, Button, Container } from '@mui/material'
-
-export default function LandingPage () {
-    return (
-        <Container maxWidth="md">
-            <Typography variant="h1">
-                Build your dream digital library with Private Library
-            </Typography>
-            <Typography variant="body1">
-                Empowering you to organize and access your book collection with ease!
-            </Typography>
-            {/* todo: add button as link to home */}
-            <Button variant="contained" color="primary">
-                Get Started
-            </Button>
-        </Container>
-    )
+import NextLink from 'next/link'
+export default function LandingPage() {
+  return (
+    <Container maxWidth="md">
+      <Typography variant="h1">
+        Build your dream digital library with Private Library
+      </Typography>
+      <Typography variant="body1">
+        Empowering you to organize and access your book collection with ease!
+      </Typography>
+      <Button component={NextLink} href="/dashboard" variant="contained" color="primary">
+        Get Started
+      </Button>
+    </Container>
+  )
 }
--- app/ui/book-form.tsx
+++ app/ui/book-form.tsx
@@ -1,22 +1,76 @@
 "use client"
 
-import React from 'react'
-import { Button, TextField, Grid } from '@mui/material';
+import { useState } from 'react';
+import { Alert, Button, Chip, Container, TextField, Stack, Box, Typography, } from '@mui/material';
+import { useFormik } from 'formik';
+import { Book } from "../../lib/types";
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+  title: Yup.string().required("Title is required"),
+  author: Yup.string().required("Author is required"),
+  genre: Yup.string().required("Genre is required"),
+  description: Yup.string()
+});
 
 export default function BookForm() {
-    const handleSubmit = () => {
-        console.log("submit handled")
-    }
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <Grid container spacing={2}>
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-               
-            </Grid>
-            <Button color="primary">Submit</Button>
-        </form>
-    )
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      author: "",
+      genre: "",
+      description: ""
+    } as Book,
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+
+      alert(JSON.stringify(values, null, 2));
+      setSuccessMessage("Your book has been successfully added")
+    }
+  });
+
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" gutterBottom>
+        Add a book:
+      </Typography>
+      <form onSubmit={formik.handleSubmit}>
+        <Stack spacing={2}>
+          <TextField
+            fullWidth
+            id="title"
+            label="Title"
+            variant="outlined"
+            {...formik.getFieldProps("title")}
+          />
+          <TextField
+            fullWidth
+            id="author"
+            label="Author"
+            variant="outlined"
+            {...formik.getFieldProps("author")}
+          />
+          <TextField
+            fullWidth
+            id="genre"
+            label="Genre"
+            variant="outlined"
+            {...formik.getFieldProps("genre")}
+          />
+          <TextField
+            fullWidth
+            id="description"
+            label="Description"
+            variant="outlined"
+            {...formik.getFieldProps("description")}
+          />
+
+          <Button color="secondary" variant="contained" type='submit'>Submit</Button>
+          {successMessage && <Alert severity="success">{successMessage}</Alert>}
+        </Stack>
+      </form>
+    </Container>
+  )
 }
